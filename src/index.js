276e8d69b84a2ceb7da554aac66c9ac72fb24b78
Willian Siqueira Nunes <williansin@gmaial.com>
2023-05-18 20:31:56 -0300

    ajuste mais um cors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import express from 'express';
 import session from 'express-session';
-import cors from 'cors';
+//import cors from 'cors';
 const app = express();
 const port = 3030;
 
 
-app.use(cors({
-  origin: [
-    'http://localhost:3030',
-    'http://127.0.0.1',
-    'https://avaliacao-modulo-back-end.onrender.com'
-  ],
-  credentials: true,
-  exposedHeaders: ['set-cookie']
-}));
+// app.use(cors({
+//   origin: [
+//     'http://localhost:3030',
+//     'http://127.0.0.1',
+//     'https://avaliacao-modulo-back-end.onrender.com'
+//   ],
+//   credentials: true,
+//   exposedHeaders: ['set-cookie']
+// }));
+
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://avaliacao-modulo-back-end.onrender.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(express.json());
 app.use(session({
   genid: () => 'id-' + (new Date()).getTime(),
