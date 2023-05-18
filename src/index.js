54581d78d80e556555eda7a76a43638bcc8b07c9
Willian Siqueira Nunes <williansin@gmaial.com>
2023-05-18 20:40:53 -0300

    Ajuste cors

--- src/index.js
+++ src/index.js
@@ -5,35 +5,10 @@ const app = express();
 const port = 3030;
 
 
-// app.use(cors({
-//   origin: [
-//     'http://localhost:3030',
-//     'http://127.0.0.1',
-//     'https://avaliacao-modulo-back-end.onrender.com'
-//   ],
-//   credentials: true,
-//   exposedHeaders: ['set-cookie']
-// }));
+app.use(cors({
+  credentials: true
+}));
 
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://avaliacao-modulo-back-end.onrender.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use(express.json());
 app.use(session({
